34995bb996cb3831dbb85b5bf19f42913d91a813
Udaya Kumar shetty <134036930+udayshetty770@users.noreply.github.com>
2025-05-06 09:33:53 +0530

    Update Program.java

--- src/com/one/Program.java
+++ src/com/one/Program.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 		System.out.println("hello word !!");
-		Sacnner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		System.out.println("The Square of "+n+" is : "+(n*n));
 	}
